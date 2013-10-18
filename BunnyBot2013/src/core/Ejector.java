384ffc6a09b37344871dc5fbaee6871bcc3be69a
warren.elwood <warren.elwood@CV-F11-56940.Student.CSD509J.Net>
2013-10-17 20:26:36 -0700

    Created Ejector class

---
+++ BunnyBot2013/src/core/Ejector.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+import edu.wpi.first.wpilibj.Solenoid;
+
+/**
+ *
+ * @author warren.elwood
+ */
+public class Ejector {
+
+    public Ejector(myJoystick joy, int port1, int port2, int port3, int port4)// Joy will come later
+    {
+        Piston sol = new Piston(int port1,int port2, int port3, int port4);
+        
+           
+          }
+    public void run() {
+        
+            if (/*TODO*/)// replace with code that gets the joystick button state
+            {
+                sol.solenoidSwitch();
+            }
+
+
+
+        }
+
+
+
+    }
+
