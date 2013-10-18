94ed6e7d879a9ab4a43612589c4ca9b70f222362
Merfoo <fauzimerfoo@gmail.com>
2013-10-17 20:40:04 -0700

    Fixed fomats and such in Ejector class.

--- BunnyBot2013/src/core/Ejector.java
+++ BunnyBot2013/src/core/Ejector.java
@@ -10,26 +10,19 @@ import edu.wpi.first.wpilibj.Solenoid;
  *
  * @author warren.elwood
  */
-public class Ejector {
-
+public class Ejector 
+{
     public Ejector(myJoystick joy, int port1, int port2, int port3, int port4)// Joy will come later
     {
-        Piston sol = new Piston(int port1,int port2, int port3, int port4);
-        
-           
-          }
-    public void run() {
-        
-            if (/*TODO*/)// replace with code that gets the joystick button state
-            {
-                sol.solenoidSwitch();
-            }
-
-
-
-        }
-
-
-
+        Piston sol = new Piston(int port1,int port2, int port3, int port4);  
     }
+    
+    public void run() 
+    {
+        if (/*TODO*/)// replace with code that gets the joystick button state
+        {
+            sol.solenoidSwitch();
+        }
+    }
+}
 
