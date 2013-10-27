ed75f79565aa6dd41c79aab9b253e737a0205f53
matthew.sundberg <matthew.sundberg@CV-F11-56940.Student.CSD509J.Net>
2013-10-27 15:26:07 -0700

    New Print, Still working on it (will do it at home) -Matthew

---
+++ BunnyBot2013/src/util/Output.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author matthew.sundberg
+ *
+ */
+public class Output {
+    int[] toPrint[];
+    public void Output(int[] toPrintInput[]){
+        toPrint[] = toPrintInput[];
+    }
+    public void Print(int id; String name; int value){/*Print statement for int*/
+        for(int count=0; count<=toPrint[].length; count++){
+            if(){
+                
+            }
+        }
+        if(){
+            System.out.print(name+": "+value);
+        }
+    }
+}
