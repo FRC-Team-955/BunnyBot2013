cac1fdf8778a0ac61cb1bfdfcf86db975ef3e598
matthew.sundberg <matthew.sundberg@CV-F11-58263.Student.CSD509J.Net>
2013-10-29 18:53:24 -0700

    Added multiple Datatype support, fixed check output thing.

--- BunnyBot2013/src/util/Output.java
+++ BunnyBot2013/src/util/Output.java
@@ -5,23 +5,81 @@
 package util;
 
 /**
- *
  * @author matthew.sundberg
  *
  */
 public class Output {
-    int[] toPrint[];
-    public void Output(int[] toPrintInput[]){
-        toPrint[] = toPrintInput[];
+    int[] toPrint;
+    public void Output(int[] toPrintInput){
+        toPrint = toPrintInput;
     }
-    public void Print(int id; String name; int value){/*Print statement for int*/
-        for(int count=0; count<=toPrint[].length; count++){
-            if(){
-                
+    public boolean checkToPrint(int idNum){
+        for(int count=0; count<=toPrint.length; count++){
+            if(idNum==toPrint[count]){
+                return true;
+            }
+        }/*End of the loop to test if is included in output array.*/
+        return false;
+    }
+    public void Print(int id, String name, int value){/*Print statement for int*/
+        if(checkToPrint(id)){
+            if(name!=""&&name!=null){
+                System.out.print(name+": "+value);
+            }
+            else {
+                System.out.print(value);
+            }
+            
+        }
+    }
+    public void Print(int id, String name, boolean value){/*Print statement for Boolean*/
+        if(checkToPrint(id)){
+            if(name!=""&&name!=null){
+                System.out.print(name+" is "+value);
+            }
+            else {
+                System.out.print(value);
+            }
+            
+        }
+    }
+    public void Print(int id, String name, double value, int roundPoint){/*Print statement for Double, optional round point*/
+        if(checkToPrint(id)){
+            if(roundPoint!=0&&roundPoint!=null){
+                double valueRounded = ;
+                if(name!=""&&name!=null){
+                    System.out.print(name+": "+valueRounded);
+                }
+                else {
+                    System.out.print(valueRounded);
+                }
             }
         }
-        if(){
-            System.out.print(name+": "+value);
-        }
     }
+    public void Print(int id, String name, double value){/*Print statement for Double sans roundpoint*/
+        if(checkToPrint(id)){
+            if(name!=""&&name!=null){
+                System.out.print(name+": "+value);
+            }
+            else {
+                System.out.print(value);
+            }
+            
+        }
+    }/*Ythak rage comment:
+             HELLO SILLY HUMANS
+             I HAVE COME TO EAT EVERYTHING YOU LOVE
+             INCLUDING YOU
+             AND YOUR MOM'
+             AND YOUR DAD
+             AND YOUR MOM'S MOM
+             AND YOUR DAD'S DAD
+             WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+             OKEY
+             hello
+             i am a destroyer
+             named may
+             hehehehehehhehehehehehhehehehehehehehe
+             weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+             okay bye :)*/
 }
