52ea7e3d80a0d12308a4c88a72723fe7e2eeea32
trevor.jones <trevor.jones@CV-F11-58200.Student.CSD509J.Net>
2013-11-03 15:28:14 -0800

    Added the Vars for the TimerEjector class

--- BunnyBot2013/src/util/Vars.java
+++ BunnyBot2013/src/util/Vars.java
@@ -13,8 +13,11 @@ package util;
 public class Vars {
     
     public static final int MAX_SPEED = 10;//feet per second
+    public static final int EJECTOR_CHANNEL = 10;
+    public static final int EJECTOR_BUTTON = 78;
+    public static final int EJECTOR_TIME = 54;
+    public static final double EJECTOR_SPEED = 0.76536;
     
-    
-    
+            
     
 }
