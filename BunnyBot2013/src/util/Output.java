12c7e80cc55ee55dd6fbe37f030b95480e12a1c3
matthew.sundberg <matthew.sundberg@CV-F11-56940.Student.CSD509J.Net>
2013-11-05 19:53:11 -0800

    Output 1.3.1415  Actually fixed it, initializer required -MS

--- BunnyBot2013/src/util/Output.java
+++ BunnyBot2013/src/util/Output.java
@@ -10,9 +10,10 @@ package util;
  */
 public class Output {
     static int[] toPrint;
-    public static void Output(int[] toPrintInput){
+    public static void initialize(int[] toPrintInput){
         toPrint = toPrintInput;
     }
+    
     public static boolean checkToPrint(int idNum){
         for(int count=0; count<=toPrint.length; count++){
             if(idNum==toPrint[count]){
@@ -21,9 +22,9 @@ public class Output {
         }/*End of the loop to test if is included in output array.*/
         return false;
     }
-    public static void Print(int id, String name, int value){/*Print statement for int*/
+    public static void print(int id, String name, int value){/*Print statement for int*/
         if(checkToPrint(id)){
-            if(name!=""&&name!=null){
+            if(name!=""){
                 System.out.print(name+": "+value);
             }
             else {
@@ -32,7 +33,7 @@ public class Output {
             
         }
     }
-    public static void Print(int id, String name, boolean value){/*Print statement for Boolean*/
+    public static void print(int id, String name, boolean value){/*Print statement for Boolean*/
         if(checkToPrint(id)){
             if(name!=""&&name!=null){
                 System.out.print(name+" is "+value);
@@ -43,7 +44,7 @@ public class Output {
             
         }
     }
-    public static void Print(int id, String name, double value){/*Print statement for Double*/
+    public static void print(int id, String name, double value){/*Print statement for Double*/
         if(checkToPrint(id)){
             if(name!=""&&name!=null){
                 System.out.print(name+": "+value);
@@ -54,7 +55,7 @@ public class Output {
             
         }
     }
-    public static void Println(int id, String name, int value){/*Println statement for int*/
+    public static void println(int id, String name, int value){/*Println statement for int*/
         if(checkToPrint(id)){
             if(name!=""&&name!=null){
                 System.out.println(name+": "+value);
@@ -65,7 +66,7 @@ public class Output {
             
         }
     }
-    public static void Println(int id, String name, boolean value){/*Println statement for Boolean*/
+    public static void println(int id, String name, boolean value){/*Println statement for Boolean*/
         if(checkToPrint(id)){
             if(name!=""&&name!=null){
                 System.out.println(name+" is "+value);
@@ -76,7 +77,7 @@ public class Output {
             
         }
     }
-    public static void Println(int id, String name, double value){/*Println statement for Double*/
+    public static void println(int id, String name, double value){/*Println statement for Double*/
         if(checkToPrint(id)){
             if(name!=""&&name!=null){
                 System.out.println(name+": "+value);
@@ -87,33 +88,33 @@ public class Output {
             
         }
     }
-    public static void Print(int id, int value){/*Print statement for int*/
+    public static void print(int id, int value){/*Print statement for int*/
         if(checkToPrint(id)){
             System.out.print(value);
         }
     }
-    public static void Print(int id, boolean value){/*Print statement for Boolean*/
+    public static void print(int id, boolean value){/*Print statement for Boolean*/
         if(checkToPrint(id)){
             System.out.print(value);
             
         }
     }
-    public static void Print(int id, double value){/*Print statement for Double*/
+    public static void print(int id, double value){/*Print statement for Double*/
         if(checkToPrint(id)){
             System.out.print(value);
         }
     }
-    public static void Println(int id, int value){/*Println statement for int*/
+    public static void println(int id, int value){/*Println statement for int*/
         if(checkToPrint(id)){
             System.out.println(value);
         }
     }
-    public static void Println(int id, boolean value){/*Println statement for Boolean*/
+    public static void println(int id, boolean value){/*Println statement for Boolean*/
         if(checkToPrint(id)){
             System.out.println(value);
         }
     }
-    public static void Println(int id, double value){/*Println statement for Double*/
+    public static void println(int id, double value){/*Println statement for Double*/
         if(checkToPrint(id)){
             System.out.println(value);
         }
