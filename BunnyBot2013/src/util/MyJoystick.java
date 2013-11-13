e15253da54c71adcf1ff92e09dadd724eb862aae
raiderbot-3 <raiderbot-3@raiderbot3-Linux>
2013-11-12 18:29:09 -0800

    Fixed everything

--- BunnyBot2013/src/util/MyJoystick.java
+++ BunnyBot2013/src/util/MyJoystick.java
@@ -10,21 +10,25 @@ import edu.wpi.first.wpilibj.Joystick;
  * @author seraj.bonakdar
  */
 public class MyJoystick extends Joystick {
+    boolean[] buttonState;
+    
     public MyJoystick(int portNumber){
         super(portNumber);
-        Output.println(7,"Joystick created with port:", portNumber);       
+        Output.println(7,"Joystick created with port:", portNumber);
+        
+        
+        
     }
     //calls constructor
     public boolean getDebounce(int button){
-           boolean lastState = getRawButton(button);
-           //make sure the button is set to false? 
-           if(lastState == false && lastState == !getRawButton(button)){
-               
+           if(buttonState[button] == false && buttonState[button] ==! getRawButton(button)){               
                Output.println(7,"Button is Pressed!",true);
+               buttonState[button] = getRawButton(button);
                return true; 
            }  
            // If the the button was unpressed and now is pressed it is pressed
            else{
+               buttonState[button] = getRawButton(button);
                return false;
            //If not the button is not pressed    
            }
@@ -35,3 +39,4 @@ public class MyJoystick extends Joystick {
    
     
 }
+//WAR-WAR IS MAD MAD 
