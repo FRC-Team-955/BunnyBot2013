b1b12542cb97204202ce63e27b50253773468420
raiderbot-3 <raiderbot-3@raiderbot3-Linux>
2013-11-19 19:48:11 -0800

    Debugging timerAuto

--- BunnyBot2013/src/auto/TimerAuto.java
+++ BunnyBot2013/src/auto/TimerAuto.java
@@ -45,6 +45,7 @@ public class TimerAuto {
                 two = true;
                 timer.stop();
                 timer.reset();
+               
                 System.out.println("Enter else");
             }
         }
