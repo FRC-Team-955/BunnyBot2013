962468fd409f0adaf479ded82bf390c8df04a8ea
raiderbot-3 <raiderbot-3@raiderbot3-Linux>
2013-11-19 19:08:26 -0800

    Fixed up VectorController/MyMath.

--- BunnyBot2013/src/core/Drive.java
+++ BunnyBot2013/src/core/Drive.java
@@ -11,6 +11,9 @@ import util.Output;
  * @author trevor.jones
  */
 public class Drive 
+/**
+ * Creates objects for each talon.
+ */
 {
     MyTalon leftTalonOne = new MyTalon(Config.chnDriveLeft1);
     //MyTalon leftTalonTwo = new MyTalon();
@@ -20,6 +23,11 @@ public class Drive
     //MyTalon rightTalonTwo = new MyTalon();
     //MyTalon rightTalonThree = new MyTalon();
      
+    /**
+     * The function makes two speeds for the motors and calls setLeft and setRight
+     * @param x     The place on the x axis of the joystick the stick is on.
+     * @param y     The place on the y axis of the joystick the stick is on.
+     */
     public void set(double x, double y) 
     {
         double left = y+x;
@@ -28,14 +36,20 @@ public class Drive
         setLeft(left);
         setRight(right);
     }
-    
+    /**
+     * This function sets double left (the left motor speed) to the left talons.
+     * @param left  The set speed for the left motor.
+     */
     private void setLeft(double left) 
     {
         leftTalonOne.set(left);
         //leftTalonTwo.set(left);
         //leftTalonThree.set(left);
     }
-    
+    /**
+     * This function sets double right (the right motor speed) to the right talons.
+     * @param right The set speed for the right motor.
+     */
     private void setRight(double right) 
     {
         rightTalonOne.set(right);
